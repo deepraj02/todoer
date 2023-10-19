343979ce790034d083a12b29122a60fa7e7c7bd4
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-10-19 17:20:14 +0530

    📦 FEAT: basic homepage layout

--- lib/src/features/home/home.page.dart
+++ lib/src/features/home/home.page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:neubrutalism_ui/neubrutalism_ui.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
   static const String pageRoute = "homepage";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Hello")),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+          child: NeuSearchBar(
+        borderColor: Colors.black,
+      )),
     );
   }
 }
